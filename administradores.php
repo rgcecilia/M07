c31c7590df095f9c1c1aa975cd1d1179fc35a2f7
rgcec <rgcec@RGC-AY135NS>
2019-01-13 22:11:50 +0100

    Actualizacion

--- administradores.php
+++ administradores.php
@@ -11,6 +11,7 @@ include("controles.php");
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="validaFormJS.js" type="text/javascript"></script>
     </head>
 
     <body>
